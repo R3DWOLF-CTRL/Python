52dd31cb007d00d2444e56afcb8d44c0455f3d8d
R3DWOLF <71910928+R3DWOLF-CTRL@users.noreply.github.com>
2020-10-30 19:46:43 -0400

    Add files via upload

---
+++ (2)Intergers and Floats - Working with numeric data.py
@@ -0,0 +1,67 @@
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+print(3+4)
+
+print(3-2)
+
+print(3*2)
+
+print(3/3)
+
+print(3//2)
+
+print(3**3)
+
+print(3 % 5)
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+num_1 = 5
+num_2 = 5
+
+print(num_1 == num_2)
+
+num_1 = 8
+num_2 = 3
+
+print(num_1 != num_2)
+
+num_1 = 23
+num_2 = 5
+
+print(num_1 > num_2)
+
+num_1 = 5
+num_2 = 8
+
+print(num_1 < num_2)
+
+num_1 = 5
+num_2 = 4
+
+print(num_1 >= num_2)
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 >= num_2)
+
+
+num_1 = 2
+num_2 = 5
+
+print(num_1 <= num_2)
